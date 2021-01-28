1079a30fe3af488ac215077a33568403c301501c
Bobby-Migs <rionjumiguel@gmail.com>
2021-01-28 15:30:41 +0800

    jan 28 update

--- APP-dev-Rental/src/app/app-routing.module.ts
+++ APP-dev-Rental/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { FindabikeComponent } from './components/findabike/findabike.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {path: "", pathMatch: "full", redirectTo: "home"},
   {path: "home", component: HomeComponent},
   {path: "findabike", component: FindabikeComponent},
+  {path: "about", component: AboutComponent}
 ];
 
 @NgModule({
